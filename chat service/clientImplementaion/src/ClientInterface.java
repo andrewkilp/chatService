8d3865e58d92cdfd3bbfb857a1431ec562d6353d
andrewkilp <andykilp96@gmail.com>
2024-04-25 14:55:26 -0500

    client after sucessfully connected
    needing to add a for loop to add components

---
+++ chat service/clientImplementaion/src/ClientInterface.java
@@ -0,0 +1,9 @@
+import java.awt.Component;
+
+import javax.swing.JFrame;
+
+public class ClientInterface extends JFrame  {
+    public ClientInterface(Component... mods) {
+        setVisible(true);
+    }
+}
