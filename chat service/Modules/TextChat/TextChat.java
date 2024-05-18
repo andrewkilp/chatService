68efa20771a131175d38531bbe92a2382d3042fa
andrewkilp <andykilp96@gmail.com>
2024-05-18 16:33:29 -0500

    Update TextChat.java
    no more errors when key is pressedd still need to decide wether or not to use object input or data input or output stream

--- chat service/Modules/TextChat/TextChat.java
+++ chat service/Modules/TextChat/TextChat.java
@@ -6,6 +6,10 @@ import java.awt.event.KeyListener;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import javax.swing.JTextField;
 
@@ -57,23 +61,61 @@ public class TextChat extends Modules{
             @Override
             public void keyTyped(KeyEvent e) {
                 // TODO Auto-generated method stub
-                throw new UnsupportedOperationException("Unimplemented method 'keyTyped'");
             }
 
             @Override
             public void keyPressed(KeyEvent e) {
-                // TODO Auto-generated method stub
-                throw new UnsupportedOperationException("Unimplemented method 'keyPressed'");
+                if(e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    String data = chatBox.getText();
+                    byte[] dataToSend = new byte[data.length()*2];
+                    for(int i = 0; i<data.length(); i++) {
+                        char c = data.charAt(i);
+                        if(i==0) {
+                            dataToSend[i] = (byte) ((byte) c&0xFF);
+                            dataToSend[i+1] = (byte) ((byte) (c>>8)&0xFF);
+                            continue;
+                        }
+                        dataToSend[i*2] = (byte) ((byte) c&0xFF);
+                        dataToSend[(i*2) +1] = (byte) ((byte) (c>>8)&0xFF);
+                        
+                    }
+                    sendData(dataToSend);
+                }
             }
 
             @Override
             public void keyReleased(KeyEvent e) {
                 // TODO Auto-generated method stub
-                throw new UnsupportedOperationException("Unimplemented method 'keyReleased'");
             }
             
         });
     }
-
-    
+    public class receiveData implements Runnable {
+        @Override
+        public void run() {
+            while(true) {
+                String string;
+                List<Object> byteData = Collections.synchronizedList(new ArrayList<>());
+                while (true) {
+                    try {
+                        byteData.add((byte) in.read());
+                    } catch (IOException ex) {
+                        break;
+                    }
+                }
+                byte[] data = new byte[byteData.size()];
+                int index = 0;
+                for (Object byteToUse : byteData) {
+                    data[index++] = (byte) byteToUse;
+                }
+                string = new String(data, StandardCharsets.UTF_8);
+                System.out.println(string);
+                try {
+                    out.write(data);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
