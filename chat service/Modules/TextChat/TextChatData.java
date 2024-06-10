6bfc549fe3d9ed7dbc44be915d34765b9751456f
andrewkilp <andykilp96@gmail.com>
2024-06-10 18:17:25 -0500

    Update TextChatData.java

--- chat service/Modules/TextChat/TextChatData.java
+++ chat service/Modules/TextChat/TextChatData.java
@@ -3,9 +3,11 @@ package Modules.TextChat;
 import java.io.Serializable;
 
 public class TextChatData implements Serializable{
+    String username;
     String data;
     int channel;
     public TextChatData(String username, String data, int channel) {
+        this.username =username;  // planned to do for custom coloring and stuff idk yet
         this.data = String.format("%s:%n%s%n", username, data);
         this.channel = channel;
     }
