3c3a28b3f11e369cc3a65b7a20a2fb6d1bbf08d5
andrewkilp <andykilp96@gmail.com>
2024-06-20 21:43:13 -0500

    Update Module.java

--- chat service/Modules/Module.java
+++ chat service/Modules/Module.java
@@ -5,8 +5,8 @@ import javax.swing.JComponent;
 import Client.Client;
 
 public abstract class Module extends JComponent {
-    public abstract void sendData(Object data);
-    public abstract void initFunctionality();
+    public abstract void sendData(Object data); // soon to be depreciated
+    public abstract void initFunctionality(); // soon to be depricated
     public abstract void connectToClient(Client client);
     public abstract void receiveData(Object o);
 }
